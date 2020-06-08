91860420d742f0edd5ab35c789aed2505eeda75a
mohahakram <you@example.com>
2020-06-08 19:24:59 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Main = () => {
+    return(
+        <div className="main">
+            <div className="upperNav">test test test</div>
+            <div className="cardWrap">
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="https://images.unsplash.com/photo-1574169411236-be48f751eb90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt=""/>
+                    </div>
+                    <div className="cardContent">
+                        <h3>Liked songs</h3>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    )
+}
+
+export default Main
