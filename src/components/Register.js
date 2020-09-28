216543c9bfe5274251a8da777e504baaca1fc207
mohahakram <you@example.com>
2020-09-28 23:49:54 +0200

    link to login page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,44 +1,75 @@
-import React from 'react';
-import { useState } from 'react';
-import { withRouter } from 'react-router-dom';
-import APIHandler from '../config/api/APIHandler';
+import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import APIHandler from "../config/api/APIHandler";
 
+import "../css/input.scss";
 
 const Register = (props) => {
-    
     const [user, setUser] = useState();
-    
-    const handleOnChange = async e => {
-        setUser( { ...user, [e.target.name]: e.target.value } )
-        console.log(user)
-    }
-    
-    const handleOnSubmit = async e => {
-        
-        e.preventDefault()
+    const [error, setError] = useState();
+
+    //listen to change in input and append the value to user
+    const handleOnChange = async (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+        console.log(user);
+    };
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
         try {
-            console.log(user)
-            await APIHandler.post('/session/register', user);
-            console.log('created');
-            props.history.push('/login')
+            console.log(user);
+            await APIHandler.post("/session/register", user)
+                .then((res) => {
+                    props.history.push("/login");
+                })
+                .catch((err) => setError(err.response.data.msg));
         } catch (err) {
-            console.log(err);
+            setError(err.response.data.msg);
         }
-    }
+    };
 
-    return(
-        <div className="inputDiv">
-            <h2>Register</h2>
-            <form onChange={handleOnChange} onSubmit={handleOnSubmit} >
-                <input type="text" name="firstName" placeholder="First name" required autoComplete="off"/>
-                <input type="text" name="lastName" placeholder="Last name" required autoComplete="off"/>
-                <input type="text" name="email" placeholder="Email" required autoComplete="off"/>
-                <input type="password" name="password" placeholder="Password" require autoComplete="off"/>
-                {/* <input type="password" name="confirmPassword" placeholder="Confirm password" require autoComplete="off"/> */}
-                <button >Register</button>
-            </form>
+    return (
+        <div className="input-main-content">
+            <div className="input-div">
+                <h2>
+                    Join us to discover talented artists and share your art!
+                </h2>
+                <form onChange={handleOnChange} onSubmit={handleOnSubmit}>
+                    <input
+                        type="text"
+                        name="userName"
+                        placeholder="User name"
+                        required
+                        autoComplete="off"
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        required
+                        autoComplete="off"
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        required
+                        autoComplete="off"
+                    />
+                    {/* <input type="password" name="confirmPassword" placeholder="Confirm password" require autoComplete="off"/> */}
+                    <button>Register</button>
+                </form>
+                <div className="error">
+                    <p>{error}</p>
+                </div>
+                <div className="fallback">
+                    <p>
+                        Already have an account? <Link to="/login">Log in</Link>
+                    </p>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default withRouter (Register);
+export default withRouter(Register);
