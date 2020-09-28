0c0ac8b2fca138c4d9e0585462d9cced3fa63707
mohahakram <you@example.com>
2020-09-29 00:01:32 +0200

    added get item from local storage

--- src/config/auth/ProtectedRoute.js
+++ src/config/auth/ProtectedRoute.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import UserContext  from './UserContext';
+import React, { useState, useEffect, useContext } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import UserContext  from './UserContext'
+import AuthContext  from './AuthContext'
+import  { GetWithExpiry }  from '../auth/LocalStorageSetup'
 
-const ProtectedRoute = ({ component: Component, ...rest}) => {
-    const {currentUser} = UserContext;
 
-    return currentUser ? (
-        < Route {...rest} render={ props => <Component {...props} />} />
-    ) : (<Redirect to='/login' />);
+export const ProtectedRoute = ({ component: Component, ...rest}) => {
+    
+    const [currentUser, setCurrentUser] = useContext(UserContext);
+    const [isAuthorized, setIsAuthorized] = useContext(AuthContext);
+    const [refresh , setRefresh] = useState(false)
+    
+
+    useEffect(() => {
+                const item =  GetWithExpiry('user_auth')
+                const checkItem = item ? (item.user ? ( setIsAuthorized(true), setCurrentUser(item.user),setRefresh(true) ) : null) : null
+    }, [refresh])
+
+    // if user in local storage and is athorized is true then render protected component 
+    return isAuthorized && currentUser ? (
+        < Route { ...rest } render={ props => < Component {...props} />} />
+    ) : ( < Redirect to='/login' /> );
 };
-
-export default ProtectedRoute;
