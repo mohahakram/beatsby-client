517357dc59546b6466aeaea56dea27357248a3d7
mohahakram <you@example.com>
2020-09-28 23:41:53 +0200

    added image for type

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
-
+// component called from beats page
 const Card = (props) => {
-    
-    const {title, text} = props
-    
-    return(
-        <div className="cardWrap">
+    const { title, text, image } = props;
+
+    return (
+        <div className="card-wrap">
             <div className="card">
-                <div className="cardImage">
-                        <img src="https://images.unsplash.com/photo-1574169411236-be48f751eb90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt=""/>
+                <div className="card-image">
+                    <img
+                        src={image}
+                        alt=""
+                    />
                 </div>
-                <div className="cardContent">
-                        <h3> {title} </h3>
-                        <p> {text} </p>
+                <div className="card-content">
+                    <h3> {title} </h3>
+                    <p> {text} </p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
