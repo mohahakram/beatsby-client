7a9dc934ebd99bf8b42520fa9650b5442a382ec4
mohahakram <you@example.com>
2020-09-28 23:47:37 +0200

    add or delete from fav and cart

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,39 +1,161 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import APIHandler from "../config/api/APIHandler";
-import Beats from "./Beats";
+import React, { useState, useEffect, useContext } from "react";
 
+import APIHandler from "../config/api/APIHandler";
+import BeatContext from "../config/context/BeatContext";
+import PlayingContext from "../config/context/PlayingContext";
+import PauseContext from "../config/context/PlayingContext";
+
+import MusicPlayer from "../components/MusicPlayer";
 import PlaylistComponent from "../components/PlaylistComponent";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
-
 const Playlist = (props) => {
+    const [currentBeat, setCurrentBeat] = useContext(BeatContext);
+    const [isPlaying, setIsPlaying] = useContext(PlayingContext);
+    const [isPaused, setIsPaused] = useContext(PauseContext);
+    const [isLoading, setIsLoading] = useState(false);
     const [playlist, setPalylist] = useState([]);
+    const [favourite, setFavourite] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [refresh, setRefresh] = useState(false);
     const pl = playlist.length > 0 ? true : false;
+    const fr = favourite ? true : false;
 
+    const fetchFavourites = async () => {
+        try {
+            await APIHandler.get("/favourite")
+                .then(
+                    async (res) =>
+                        res.status === 200 && (await setFavourite(res.data))
+                )
+                .then(setIsLoading(false));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const fetchCart = async () => {
+        try {
+            await APIHandler.get("/cart")
+                .then(
+                    async (res) =>
+                        res.status === 200 && (await setCart(res.data))
+                )
+                .then(setIsLoading(false));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
     useEffect(() => {
-        console.log(props.match.params.id);
         const data = async () => {
             try {
-                await APIHandler.get(`/playlist/${props.match.params.id}`).then(
-                    async (res) => await setPalylist(res.data)
-                );
+                setIsLoading(true);
+                await APIHandler.get(`/playlist/${props.match.params.id}`)
+                    .then(async (res) => {await setPalylist(res.data); console.log(res)})
+                    .then(fetchFavourites())
+                    .then(fetchCart())
+                    .then(setRefresh(false))
+                    
+                // setIsLoading(false);
                 // console.log(done);
             } catch (err) {
                 console.log(err);
             }
         };
         data();
-    }, []);
+    }, [refresh]);
+    console.log("cart:", cart);
+    console.log("fav:", favourite);
+
+    // set clicked beat as current beat 
+    // and use it in different components
+    const handleOnClickSetCurrentBeat = (data) => {
+        setCurrentBeat(data);
+    };
+
+    // function passed as prop 
+    const handleOnClickIsPlaying = () => {
+        setIsPlaying(true);
+    };
+
+    // function passed as prop
+    const handleOnClickIsPaused = () => {
+        setIsPaused(true);
+    };
+    
+    const handleOnClickAddFavourite = async (id) => {
+        try {
+            await APIHandler.post(`/favourite/add/${id}`).then((res) => {
+                // refresh changed part so the user 
+                //can see it has been added 
+                res.status === 200 && setRefresh(true);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
+    const handleOnClickDeleteFavourite = async (id) => {
+        try {
+            await APIHandler.post(`/favourite/delete/${id}`).then((res) => {
+                // refresh changed part so the user 
+                //can see it has been added 
+                res.status === 200 && setRefresh(true);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleOnClickAddToCart = async (id) => {
+        try {
+            await APIHandler.post(`/cart/add/${id}`).then((res) => {
+                res.status === 200 && setRefresh(true);
+            });
+            console.log("ok");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
+    const handleOnClickDeleteFromCart = async (id) => {
+        try {
+            await APIHandler.post(`/cart/delete/${id}`).then((res) => {
+                res.status === 200 && setRefresh(true);
+                ;
+            });
+            console.log("ok");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
-        <div> { pl && 
-            < PlaylistComponent passedData = {playlist} />}
-         </div>
-    )
+        <div className="playlist">
+            
+            {/* check if playlist and faourites variables 
+                contain items before rendering */}
+            {pl && fr ? (
+                // fragment syntax
+                <>
+                    < PlaylistComponent beatDetails = {playlist}
+                                        currentBeat={currentBeat}
+                                        isPlaying={isPlaying}
+                                        onClickIsPlaying={handleOnClickIsPlaying}
+                                        onClickIsPaused={handleOnClickIsPaused}
+                                        favouritesList={favourite.favouriteList}
+                                        cartList={cart.cartList}
+                                        onClickAddFavourite={handleOnClickAddFavourite}
+                                        onClickDeleteFavourite={handleOnClickDeleteFavourite}
+                                        onClickAddToCart={handleOnClickAddToCart}
+                                        onClickDeleteFromCart={handleOnClickDeleteFromCart}
+                                        onClickSetCurrentBeat={handleOnClickSetCurrentBeat}
+                    />
+                    < MusicPlayer />
+                </>
+            ) : null}
+        </div>
+    );
 };
 
 export default Playlist;
