290d2367693e4ee15518f8b9ad5f1ff54de39299
mohahakram <you@example.com>
2020-10-12 16:26:34 +0200

    completed music controls

--- src/components/MusicControls.js
+++ src/components/MusicControls.js
@@ -1,41 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom'
 import {Howl, Howler} from 'howler';
-import APIHandler from '../config/api/APIHandler';
-import { useState } from 'react';
-import { useEffect } from 'react';
+// import APIHandler from '../config/api/APIHandler';
 import BeatContext from '../config/context/BeatContext';
+import PlaylistContext from '../config/context/PlaylistContext';
 import PlayingContext from '../config/context/PlayingContext';
 import PauseContext from '../config/context/PauseContext';
 
 import MusicPlayer from './MusicPlayer';
 
-import '../css/musicControls.scss'
+import '../css/musicControls.scss';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle, faPauseCircle, faStepForward, faStepBackward} from "@fortawesome/free-solid-svg-icons";
+import CurrentPlaylist from '../config/context/PlaylistContext';
 
 const MusicControls =  (props) => {
 
     const [data, setData] = useState();
     const [currentBeat, setCurrentBeat] = useContext(BeatContext);
+    const [currentPlaylist, setCurrentPlaylist] = useContext(PlaylistContext);
+    const [isPlaying, setIsPlaying] = useContext(PlayingContext);
+    const [isPaused, setIsPaused] = useContext(PauseContext);
     const [volume, setVolume] = useState(1);
 
 
+    const handleOnClickIsPlaying = () => {
+        const play = isPlaying ? (setIsPaused(true),setIsPlaying(true))
+        :
+        setIsPlaying(true)
+    };
+
+    const handleOnClickIsPaused = () => {
+        setIsPaused(true);
+        setIsPlaying(false);
+    };
     
-    const handleOnPrevious = () => {
+    const handleOnNext = () => {
+        let beatIndex = currentPlaylist.indexOf(currentBeat);
+        let nextIndex = beatIndex + 1 < currentPlaylist.length ?  currentPlaylist[beatIndex + 1] : currentPlaylist[0];
+        setCurrentBeat(nextIndex);
     }
 
-    const handleOnNext = () => {
-        
+    const handleOnPrevious = () => {
+        let beatIndex = currentPlaylist.indexOf(currentBeat);
+        let previousIndex = beatIndex !== 0 ? currentPlaylist[beatIndex - 1] : currentPlaylist[0];
+        setCurrentBeat(previousIndex);
     }
 
     const handleVolumeChange = (event) => {
         setVolume((parseInt(event.target.value) / 100 ))
         Howler.volume(volume)
     }
-    
+
     return(
         <div className="music-controls">
             {currentBeat ? (
@@ -74,13 +92,18 @@ const MusicControls =  (props) => {
                     e.preventDefault()
                     handleOnPrevious()} }><FontAwesomeIcon icon={faStepBackward} fixedWidth />
                 </div>
-                { !props.isPlaying ?
-                    <div className="play-button" onClick={props.onClickPlay}>
-                        <FontAwesomeIcon icon={faPlayCircle} fixedWidth />
-                    </div>
+                { !isPlaying ?
+                    currentBeat ? 
+                        <div className="play-button" onClick={handleOnClickIsPlaying}>
+                            <FontAwesomeIcon icon={faPlayCircle} fixedWidth />
+                        </div>
+                        :
+                        <div className="play-button-disabled">
+                            <FontAwesomeIcon icon={faPlayCircle} fixedWidth />
+                        </div>
                     : 
                     
-                    <div className="pause-button" onClick={props.onClickPause}>
+                    <div className="pause-button" onClick={handleOnClickIsPaused}>
                         <FontAwesomeIcon icon={faPauseCircle} fixedWidth />
                     </div>
                 }
@@ -91,6 +114,9 @@ const MusicControls =  (props) => {
             </div>
             <div className="volume-control">
                 <input type="range" min="0" max="100"  value={volume * 100} onChange={handleVolumeChange}/>
+            </div>
+            <div className="music-player">
+                < MusicPlayer />
             </div>
         </div>
     )
