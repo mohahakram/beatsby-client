d6de6a8ac5c5ebcdd3cd1059cbf990e442bd1f96
mohahakram <you@example.com>
2020-10-27 23:25:49 +0100

    added play controls

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import "../css/cart.scss";
+
+import BeatContext from "../config/context/BeatContext";
+import PlaylistContext from "../config/context/PlaylistContext";
+import PlayingContext from "../config/context/PlayingContext";
+import PauseContext from "../config/context/PlayingContext";
 
 import APIHandler from "../config/api/APIHandler";
 import CheckoutForm from "./CheckoutForm";
@@ -14,6 +19,10 @@ import PlaylistComponent from "./PlaylistComponent";
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const Cart = () => {
+    const [currentBeat, setCurrentBeat] = useContext(BeatContext);
+    const [currentPlaylist, setCurrentPlaylist] = useContext(PlaylistContext);
+    const [isPlaying, setIsPlaying] = useContext(PlayingContext);
+    const [isPaused, setIsPaused] = useContext(PauseContext);
     const [cart, setCart] = useState();
     const [cartTotalItems, setCartTotalItems] = useState(0);
     const [cartTotalPrice, setCartTotalPrice] = useState(0);
@@ -43,6 +52,26 @@ const Cart = () => {
         } catch (err) {
             console.log(err);
         }
+    };
+
+    // set clicked beat as current beat 
+    // and use it in different components
+    const handleOnClickSetCurrentBeat = (data) => {
+        setCurrentBeat(data);
+    };
+
+    const handleOnClickSetCurrentPlaylist = () => {
+        setCurrentPlaylist(cart)
+    }
+
+    // function passed as prop 
+    const handleOnClickIsPlaying = () => {
+        setIsPlaying(true);
+    };
+
+    // function passed as prop
+    const handleOnClickIsPaused = () => {
+        setIsPaused(true);
     };
 
     return (
@@ -76,14 +105,19 @@ const Cart = () => {
                     <h2>{cartTotalItems} item in cart</h2>
                 )}
                 {/* if cart has item show component */}
-                {cartTotalItems > 0 && (
+                {cartTotalItems > 0 && cart.length > 0 ? (
                     <div className="playlist">
-                        <PlaylistComponent
-                            beatDetails={cart}
-                            onClickDeleteFromCart={handleDeleteFromCart}
+                        <PlaylistComponent  beatDetails={cart}
+                                            onClickDeleteFromCart={handleDeleteFromCart}
+                                            currentBeat={currentBeat}
+                                            isPlaying={isPlaying}
+                                            onClickIsPlaying={handleOnClickIsPlaying}
+                                            onClickIsPaused={handleOnClickIsPaused}
+                                            onClickSetCurrentPlaylist={handleOnClickSetCurrentPlaylist}
+                                            onClickSetCurrentBeat={handleOnClickSetCurrentBeat}
                         />
                     </div>
-                )}
+                ) : null }
             </div>
         </div>
     );
